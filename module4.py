93f0ca09cf2d38f7fec42131dcb1babf190d93c2
misha <grut5391@gmail.com>
2023-12-12 16:50:02 +0200

    module4

---
+++ module4.py
@@ -0,0 +1,78 @@
+# num1 = int(input("Enter your 1 number :"))
+# num2 = int(input("Enter your 2 number :"))
+
+# sum1 = 1
+
+# for i in range(num1,num2):
+#     sum1 += 1
+#     if sum1 % 7 == 0 :
+#             print(sum1)
+#     else:
+#         continue
+
+# ======================================================================================================================================
+
+
+
+
+# num1 = int(input("Enter your 1 number :"))
+# num2 = int(input("Enter your 2 number :"))
+
+# str = ""
+# sum1 = 1
+# sum2 = 0
+# for i in range(1):
+    
+#     for j in range(num1,num2+1):
+#         str+= f"{j},"
+#     print(str)
+   
+# for e in range(1):
+#     str = ""
+#     for y in range(num1,num2+1):
+#         str+= f"{y},"
+#     print(str[::-1])
+   
+# for v in range(1):
+#     str = ""
+#     for s in range(num1,num2+1):
+#         sum1 += 1
+#         if sum1 % 7 == 0 :
+#              str+= f"{sum1},"
+#         else:
+#          continue
+        
+#     print(str)
+# for a in range(1):
+#     str = ""
+#     for f in range(num1,num2+1):
+#         sum2 += 1
+#         if sum2 // 5 == 0 :
+#                 str+= f"{sum2},"
+#         else:
+#             continue
+
+#     print(str)
+
+
+# ====================================================================================================================
+
+# num1 = int(input("Enter your 1 number :"))
+# num2 = int(input("Enter your 2 number :"))
+
+# sum1 = 0
+
+# for i in range(num1,num2+1):
+#     sum1+=1
+#     if sum1 % 3 == 0 and sum1 % 5 == 0:
+#         print(f"fizz Buzz {sum1}")
+#         continue
+#     if sum1 % 3 == 0:
+#         print(f"Fizz {sum1}")
+#         continue
+#     if sum1 % 5 == 0:
+#         print(f"Buzz {sum1}")
+#         continue
+#     else:
+#         print(sum1)
+    
