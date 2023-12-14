78d35d35b867acbe0223d01d8a468fad958a318c
misha <grut5391@gmail.com>
2023-12-14 14:51:01 +0200

    module 5

---
+++ module5.py
@@ -0,0 +1,47 @@
+# str1 = input("Введіть слово :")
+
+# if str1[::] == str1[::-1]:
+#     print("це слово паліндром")
+# else:
+#     print("Це не паліндром")
+    
+    
+# ======================================================================================================================================
+    
+# str1 = input("Введіть слово :")   
+
+# m =['none','with','assert','break','continue','class','def','del','if','elif','else','except','finally','while']    
+
+# splitsText = str1.split()
+
+# count1 = 0
+# str2 = ""
+# for i in range(len(splitsText)):
+#     if splitsText[i] in m:
+#         splitsText[i] = splitsText[i].upper()
+        
+        
+# modificateText = " ".join(splitsText)
+# print(modificateText)
+
+# ======================================================================================================================================
+
+# str1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
+
+
+
+
+
+# text = str1.count(".")
+# print(text)
+
+
+
+        
+        
+        
+
+
+
+
+
